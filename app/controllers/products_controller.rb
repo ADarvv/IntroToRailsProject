5eb4f7c24a2aa061115909caf8a136adcb4922a2
ADarvv <adarvish@rrc.ca>
2025-07-25 23:18:27 -0500

    Add hierarchical search by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.includes(:category).active
+    @categories = Category.all
+    
+    # Hierarchical search by category
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+    
+    # Simple search functionality 
+    if params[:search].present?
+      @products = @products.where("name LIKE ? OR description LIKE ?", 
+                                  "%#{params[:search]}%", "%#{params[:search]}%")
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def search
+    redirect_to products_path(search: params[:search], category_id: params[:category_id])
   end
 end
