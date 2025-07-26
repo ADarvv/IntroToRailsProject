25e40d265eeb02434363675d32450188cb9ca4f4
ADarvv <adarvish@rrc.ca>
2025-07-25 23:27:16 -0500

    Add simple customer views and improve product show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
   root 'products#index'
   resources :products, only: [:index, :show] do
     collection do
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   end
   resources :categories, only: [:index, :show]
   get 'about', to: 'pages#about'
+
+  resources :users, only: [:index, :show]
 end
